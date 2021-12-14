a6b58fb763b05871c38865c95dcb055e209ac2ee
simple <123@mail.ru>
2021-12-14 19:25:13 +0500

    main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import CryptRsa
 import algorthym
+
+# Создаем сессию класса с путем – исходный файл
 root = "test.txt"
-# создает сессию класса с путем
 session = CryptRsa.RSA(input_file=root)
 
 # Генерируем массив простых чисел для полу
@@ -9,7 +10,22 @@ gen_list = algorthym.GenerateSimpleNumbers()
 mass_simple_numbers = gen_list.GenerateList(count_number=100, start_number=17)
 simple_numbers = gen_list.GenerateList(count_number=1000, start_number=3)
 
+# Генерация ключей
 session.Generate_PP_keys(
     simple_numbers_list=mass_simple_numbers,
     bloc_simple_numbers=simple_numbers
+)
+
+# Кодирование файла
+session.EncodeFile(
+    root_public_key= "public key.txt"
+)
+
+# Создаем сессию класса с путем – закодированный файл
+root = "encode.txt"
+session = CryptRsa.RSA(input_file=root)
+
+# Декодирование файла 
+session.DecodeFile(
+    root_privite_key="privite key.txt"
 )
