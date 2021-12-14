0efaa721f57ad35ac0552ba970a1dfb5758dcb7d
simple <123@mail.ru>
2021-12-14 19:25:03 +0500

    main

--- CryptRsa.py
+++ CryptRsa.py
@@ -1,8 +1,9 @@
-from os import stat_result
 import random
 
 class RSA:
+    """
 
+    """
     def __init__(self, input_file : str):
         self.file_session = open(input_file)
         self.string_from_file = ""
@@ -12,6 +13,9 @@ class RSA:
     # TODO: #1 Написать генерацию публичного и приватного ключа 
 
     def gcd_extended(self, num1, num2):
+        """
+        
+        """
         if num1 == 0:
             return (num2, 0, 1)
         else:
@@ -19,6 +23,9 @@ class RSA:
         return (div, y - (num2 // num1) * x, x)
 
     def Generate_PP_keys(self, simple_numbers_list : list, bloc_simple_numbers : list):
+        """
+        
+        """
         self.p = 0
         self.q = 0
         while self.p == self.q:
@@ -41,4 +48,24 @@ class RSA:
         self.pu_key.close()
         self.pr_key.close()
         
-
+    def EncodeFile(self, root_public_key : str):
+        """
+        
+        """
+        self.enc_file = open(root_public_key, "r", encoding='UTF-8')
+        self.e , self.n = map(int,self.enc_file.readline().split(" "))
+        self.enc_message = pow(int(self.string_from_file), self.e, self.n)
+        self.enc_message_root = open("encode.txt", "w", encoding="UTF-8")
+        self.enc_message_root.write(str(self.enc_message))
+        self.enc_message_root.close()
+        
+    def DecodeFile(self,root_privite_key : str):
+        """
+        
+        """
+        self.dec_file = open(root_privite_key, "r", encoding='UTF-8')
+        self.d , self.n = map(int,self.dec_file.readline().split(" "))
+        self.dec_message = pow(int(self.string_from_file), self.d, self.n)
+        self.dec_message_root = open("decode.txt", "w", encoding="UTF-8")
+        self.dec_message_root.write(str(self.dec_message))
+        self.dec_message_root.close()
