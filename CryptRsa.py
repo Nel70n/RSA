879f4dd6e2830a2c5af76a4908fb5cbb8a56956f
simple <123@mail.ru>
2021-12-14 18:59:45 +0500

    main

--- CryptRsa.py
+++ CryptRsa.py
@@ -1,3 +1,6 @@
+from os import stat_result
+import random
+
 class RSA:
 
     def __init__(self, input_file : str):
@@ -5,4 +8,37 @@ class RSA:
         self.string_from_file = ""
         for line in self.file_session:
             self.string_from_file += line.replace('\n', '').replace(' ', '').lower()
+
+    # TODO: #1 Написать генерацию публичного и приватного ключа 
+
+    def gcd_extended(self, num1, num2):
+        if num1 == 0:
+            return (num2, 0, 1)
+        else:
+            div, x, y = self.gcd_extended(num2 % num1, num1)
+        return (div, y - (num2 // num1) * x, x)
+
+    def Generate_PP_keys(self, simple_numbers_list : list, bloc_simple_numbers : list):
+        self.p = 0
+        self.q = 0
+        while self.p == self.q:
+            self.p = simple_numbers_list[random.randint(0,len(simple_numbers_list))]
+            self.q = simple_numbers_list[random.randint(0,len(simple_numbers_list))]
         
+        self.n = self.p * self.q
+        self.phi = (self.p - 1)*(self.q - 1)
+        for num in bloc_simple_numbers:
+            if self.gcd_extended(self.phi, num)[0] == 1:
+                self.e = num
+                break
+        
+        self.d = pow( self.e, -1,  self.phi )
+        self.pr_key = open("privite key.txt", "w", encoding="UTF-8")
+        self.pu_key = open("public key.txt", "w", encoding="UTF-8")
+
+        self.pu_key.write(str(self.e) + ' ' + str(self.n))
+        self.pr_key.write(str(self.d) + ' ' + str(self.n))
+        self.pu_key.close()
+        self.pr_key.close()
+        
+
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import CryptRsa
 import algorthym
 root = "test.txt"
+# создает сессию класса с путем
 session = CryptRsa.RSA(input_file=root)
 
-mass_simple_numbers = algorthym.GenerateSimpleNumbers()
-mass_simple_numbers = mass_simple_numbers.GenerateList(count_number=10000, start_number=17)
-print(len(mass_simple_numbers))
+# Генерируем массив простых чисел для полу
+gen_list = algorthym.GenerateSimpleNumbers()
+mass_simple_numbers = gen_list.GenerateList(count_number=100, start_number=17)
+simple_numbers = gen_list.GenerateList(count_number=1000, start_number=3)
+
+session.Generate_PP_keys(
+    simple_numbers_list=mass_simple_numbers,
+    bloc_simple_numbers=simple_numbers
+)
