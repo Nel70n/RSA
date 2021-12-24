f4a5827879c82571e9c3a04429d8b112e001d524
simple <123@mail.ru>
2021-12-24 13:14:20 +0500

    main

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ gen_list = algorthym.GenerateSimpleNumbers()
 mass_simple_numbers = gen_list.GenerateList(count_number=100, start_number=17)
 simple_numbers = gen_list.GenerateList(count_number=1000, start_number=3)
 
-"""
 # Генерация ключей
 session.Generate_PP_keys(
     simple_numbers_list=mass_simple_numbers,
@@ -30,12 +29,14 @@ session = CryptRsa.RSA(input_file=root)
 session.DecodeFile(
     root_privite_key="privite key.txt"
 )
-"""
+
+# Создание электронной подписи
 a = session.DigitalSignature(
     privite_key_root="cp privite key.txt"
 )
 
-
+# Проверка личности, а именно декодирование 
+# электронной подписи
 DS = 'DigitalSignature.txt'
 session = CryptRsa.RSA(input_file=DS)
 session.EncodeFile(
